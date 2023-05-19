4d2c10c2f5546803d1426ad64a3a1fe09ae85058
n.grishkin <n.grishkin@iva.ru>
2023-05-19 10:05:39 +0500

    sps-simple-demo uses the prototype of new api

--- src/docs/examples/sps-simple-demo/sps-simple-demo.ts
+++ src/docs/examples/sps-simple-demo/sps-simple-demo.ts
@@ -1,19 +1,13 @@
 import {
-  ScrollRise,
-  StaticActor,
-  TimeFrame,
-  MoveMotion,
-  SizeMotion,
-  OpacityMotion,
-  StickyPlatformScene,
   Value,
+  scrollRise,
+  stickyPlatformScene,
+  timeFrame,
+  moveMotion,
+  sizeMotion,
+  opacityMotion,
+  staticActor,
 } from "scroll-rise";
-
-const firstBlock = new StaticActor(document.getElementById('first-block')!);
-const secondBlock = new StaticActor(document.getElementById('second-block')!);
-const thirdBlock = new StaticActor(document.getElementById('third-block')!);
-const fourthBlock = new StaticActor(document.getElementById('fourth-block')!);
-const agenda = new StaticActor(document.getElementById('agenda')!);
 
 const sceneHeight: Value = (w, h) => (h * 2);
 
@@ -28,108 +22,129 @@ const secondHalfBlockY: Value = (w, h) => h/8 * 3 - blockHeight(w, h)/2;
 
 const endBlockWidth: Value = (w, h) => w/2 - firstHalfBlockX(w, h);
 
-const hideBlockFrame = new TimeFrame(new OpacityMotion({
+const hideBlockFrame = timeFrame(opacityMotion({
   start: () => 1,
   end: () => 0,
 }), (w, h) => 0, (w, h) => h/2);
-
-firstBlock.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: firstHalfBlockX,
-    endX: firstHalfBlockX,
-    startY: firstHalfBlockY,
-    endY: firstHalfBlockY,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new SizeMotion({
-    startWidth: blockWidth,
-    startHeight: blockHeight,
-    endWidth: endBlockWidth,
-    endHeight: blockHeight,
-  }), (w, h) => 0, (w, h) => h/2),
-  hideBlockFrame,
-]);
-
-secondBlock.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: secondHalfStartBlockX,
-    endX: secondHalfEndBlockX,
-    startY: firstHalfBlockY,
-    endY: firstHalfBlockY,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new SizeMotion({
-    startWidth: blockWidth,
-    startHeight: blockHeight,
-    endWidth: endBlockWidth,
-    endHeight: blockHeight,
-  }), (w, h) => 0, (w, h) => h/2),
-  hideBlockFrame,
-]);
-
-thirdBlock.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: firstHalfBlockX,
-    endX: firstHalfBlockX,
-    startY: secondHalfBlockY,
-    endY: secondHalfBlockY,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new SizeMotion({
-    startWidth: blockWidth,
-    startHeight: blockHeight,
-    endWidth: endBlockWidth,
-    endHeight: blockHeight,
-  }), (w, h) => 0, (w, h) => h/2),
-  hideBlockFrame,
-]);
-
-fourthBlock.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: secondHalfStartBlockX,
-    endX: secondHalfEndBlockX,
-    startY: secondHalfBlockY,
-    endY: secondHalfBlockY,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new SizeMotion({
-    startWidth: blockWidth,
-    startHeight: blockHeight,
-    endWidth: endBlockWidth,
-    endHeight: blockHeight,
-  }), (w, h) => 0, (w, h) => h/2),
-  hideBlockFrame,
-]);
 
 const agendaStartWidth: Value = (w, h) => w/4;
 const agendaStartHeight: Value = (w, h) => h/4;
 const agendaEndWidth: Value = (w, h) => w/2;
 const agendaEndHeight: Value = (w, h) => h/2;
 
-agenda.addFrames([
-  new TimeFrame(new MoveMotion({
-    startX: (w, h) => w/2 - agendaStartWidth(w, h)/2,
-    endX: (w, h) => w/2 - agendaEndWidth(w, h)/2,
-    startY: (w, h) => h/4 - agendaStartHeight(w, h)/2,
-    endY: (w, h) => h/4 - agendaEndHeight(w, h)/2,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new SizeMotion({
-    startWidth: agendaStartWidth,
-    startHeight: agendaStartHeight,
-    endWidth: agendaEndWidth,
-    endHeight: agendaEndHeight,
-  }), (w, h) => 0, (w, h) => h/2),
-  new TimeFrame(new OpacityMotion({
-    start: () => 0,
-    end: () => 1,
-  }), (w, h) => 0, (w, h) => h/2),
-]);
-
-const scene = new StickyPlatformScene(
+scrollRise(stickyPlatformScene(
   document.getElementById('scene')!,
-  sceneHeight,
-);
-
-scene.add(firstBlock);
-scene.add(secondBlock);
-scene.add(thirdBlock);
-scene.add(fourthBlock);
-scene.add(agenda);
-
-const sr = new ScrollRise(scene);
+  sceneHeight
+)).operate([
+  staticActor(document.getElementById('first-block')!).use([
+    timeFrame(
+      moveMotion({
+        startX: firstHalfBlockX,
+        endX: firstHalfBlockX,
+        startY: firstHalfBlockY,
+        endY: firstHalfBlockY,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      sizeMotion({
+        startWidth: blockWidth,
+        startHeight: blockHeight,
+        endWidth: endBlockWidth,
+        endHeight: blockHeight,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    hideBlockFrame,
+  ]),
+  staticActor(document.getElementById('second-block')!).use([
+    timeFrame(
+      moveMotion({
+        startX: secondHalfStartBlockX,
+        endX: secondHalfEndBlockX,
+        startY: firstHalfBlockY,
+        endY: firstHalfBlockY,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      sizeMotion({
+        startWidth: blockWidth,
+        startHeight: blockHeight,
+        endWidth: endBlockWidth,
+        endHeight: blockHeight,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    hideBlockFrame,
+  ]),
+  staticActor(document.getElementById('third-block')!).use([
+    timeFrame(
+      moveMotion({
+        startX: firstHalfBlockX,
+        endX: firstHalfBlockX,
+        startY: secondHalfBlockY,
+        endY: secondHalfBlockY,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      sizeMotion({
+        startWidth: blockWidth,
+        startHeight: blockHeight,
+        endWidth: endBlockWidth,
+        endHeight: blockHeight,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    hideBlockFrame,
+  ]),
+  staticActor(document.getElementById('fourth-block')!).use([
+    timeFrame(
+      moveMotion({
+        startX: secondHalfStartBlockX,
+        endX: secondHalfEndBlockX,
+        startY: secondHalfBlockY,
+        endY: secondHalfBlockY,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      sizeMotion({
+        startWidth: blockWidth,
+        startHeight: blockHeight,
+        endWidth: endBlockWidth,
+        endHeight: blockHeight,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    hideBlockFrame,
+  ]),
+  staticActor(document.getElementById('agenda')!).use([
+    timeFrame(
+      moveMotion({
+        startX: (w, h) => w/2 - agendaStartWidth(w, h)/2,
+        endX: (w, h) => w/2 - agendaEndWidth(w, h)/2,
+        startY: (w, h) => h/4 - agendaStartHeight(w, h)/2,
+        endY: (w, h) => h/4 - agendaEndHeight(w, h)/2,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      sizeMotion({
+        startWidth: agendaStartWidth,
+        startHeight: agendaStartHeight,
+        endWidth: agendaEndWidth,
+        endHeight: agendaEndHeight,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+    timeFrame(
+      opacityMotion({
+        start: () => 0,
+        end: () => 1,
+      }),
+      (w, h) => 0, (w, h) => h/2,
+    ),
+  ]),
+]);
