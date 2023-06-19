9ed1d06e07aaa7195e3d4cef51eb87e59e5baaec
n.grishkin <n.grishkin@iva.ru>
2023-06-19 18:04:38 +0500

    generation

---
+++ src/generated/spec-wrappers/Fixed-Actors-Scene-offset-test-should-have-correct-X,-Y-coords-in-changing-X.ts
@@ -0,0 +1,6 @@
+declare var LIB_PATH: string | undefined;
+
+import('../../spec-examples-generator/spec-jit-proxy').then((proxy) => {
+  proxy.runTest('Fixed Actors Scene: offset test', 'should have correct X, Y coords in changing X');
+  import(`${LIB_PATH}/../../../scroll-rise/src/specs/offset/fas-offset.spec.ts`);
+});
