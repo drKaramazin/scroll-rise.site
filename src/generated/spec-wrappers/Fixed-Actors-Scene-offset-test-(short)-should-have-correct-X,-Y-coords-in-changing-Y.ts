83c80e08a8e16ac2fa5e49f3a8e0ca78406a3b31
n.grishkin <n.grishkin@iva.ru>
2023-07-05 19:47:48 +0500

    fas and sps short move tests. fas and sps offset short tests.

---
+++ src/generated/spec-wrappers/Fixed-Actors-Scene-offset-test-(short)-should-have-correct-X,-Y-coords-in-changing-Y.ts
@@ -0,0 +1,6 @@
+declare var LIB_PATH: string | undefined;
+
+import('../../spec-examples-generator/spec-jit-proxy').then((proxy) => {
+  proxy.runTest('Fixed Actors Scene: offset test (short)', 'should have correct X, Y coords in changing Y');
+  import(`${LIB_PATH}/../../../scroll-rise/src/specs/offset/fas-offset-short.spec.ts`);
+});
